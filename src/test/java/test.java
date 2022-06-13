5b969044c1dbbde361afabbc584f83c6a2612782
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-06-13 14:46:06 +0200

    xd

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,21 @@
+import helpers.Helpers;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class test {
+    WebDriver driver;
+    Helpers helper = new Helpers();
 
     @Test
     public void mainTest(){
@@ -26,11 +35,43 @@ public class test {
         opt.addArguments("start-maximized");
         opt.addArguments("--disable-gpu");
         opt.addArguments("--disable-setuid-sandbox");
-        WebDriver driver = new ChromeDriver(opt);
+        driver = new ChromeDriver(opt);
 
-        driver.get("https://google.com");
+        String keyword = "test";
+        String url = "http://bip.piekary.pl/?c=179"
+        driver.get(url);
         System.out.println("Title: " + driver.getTitle());
 
+        reporter_7(url, keyword);
 
+    }
+
+    public int countSelects(String keyWord){
+        String formatedXPATH = String.format("//a[contains(text(), '%s') or contains(text(), '%s') or contains(text(), '%s')]", keyWord, keyWord.toLowerCase(Locale.ROOT), keyWord.toUpperCase(Locale.ROOT));
+        List<WebElement> elementList = driver.findElements(By.xpath(formatedXPATH));
+        return elementList.size();
+    }
+
+    public void reporter_7(String URL, String keyWord){
+
+        String outputFile = (helper.todayDateFromPattern("dd-MM-yyyy") + "_przetargi.html");
+        String style = "style='border: 1px solid black; padding: 3px; text-align: center;'";
+        try {
+            FileWriter txtOutput = new FileWriter(outputFile, true);
+
+            if(countSelects(keyWord) > 0)
+            {
+                txtOutput.write("<table " + style + ">");
+                txtOutput.write("<tr " + style + "><th "+ style + ">Wynik z dnia i godziny</th><th " + style + ">Link</th><th " + style + ">Fraza</th><th " + style + ">Ilość</th></tr>");
+                txtOutput.write("<tr " + style +"><td " + style + ">" + helper.todayDateFromPattern("HH:mm:ss dd-MM-yyyy")  + "</td><td "+ style + ">" + "<a target='_blank' href=" + URL + ">" + URL +"</a>" + "</td><td " + style+">" + keyWord +"</td><td " + style+ ">" + countSelects(keyWord) + " </td></tr>");
+                txtOutput.write("</table>");
+                txtOutput.write("</br>");
+
+                txtOutput.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

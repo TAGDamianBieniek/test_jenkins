4c6441c9cb6edda0f815970419a6dc208f26b83e
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-06-24 14:37:16 +0200

    fixyy

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -14,21 +14,21 @@ public class test {
         System.out.println(x);
         WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
-        File file = new File("/usr/local/bin/chromedriver");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//        File file = new File("/usr/local/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
 
 
         ChromeOptions opt = new ChromeOptions();
-//        opt.setBinary("/usr/bin/google-chrome");
+        opt.setBinary("/usr/bin/google-chrome");
 //
-        opt.addArguments("--headless");
+//        opt.addArguments("--headless");
 //        opt.addArguments("--window-size=1280x800");
 //        opt.addArguments("--no-sandbox");
 //        opt.addArguments("–-disable-dev-shm-usage");
 //        opt.addArguments("--start-maximized");
 //        opt.addArguments("--disable-gpu");
 //        opt.addArguments("--disable-setuid-sandbox");
-        WebDriver driver = new ChromeDriver(opt);
+//        WebDriver driver = new ChromeDriver(opt);
 
 
 //

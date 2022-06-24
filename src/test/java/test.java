311546b12e5737cb787dadb99638004836a0718e
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-06-24 10:24:02 +0200

    fix

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -29,12 +29,12 @@ public class test {
         opt.setBinary("/usr/bin/google-chrome");
 
         opt.addArguments("--headless");
-        opt.addArguments("--window-size=1280x800");
-        opt.addArguments("--no-sandbox");
-        opt.addArguments("–disable-dev-shm-usage");
-        opt.addArguments("--start-maximized");
-        opt.addArguments("--disable-gpu");
-        opt.addArguments("--disable-setuid-sandbox");
+//        opt.addArguments("--window-size=1280x800");
+//        opt.addArguments("--no-sandbox");
+//        opt.addArguments("–disable-dev-shm-usage");
+//        opt.addArguments("--start-maximized");
+//        opt.addArguments("--disable-gpu");
+//        opt.addArguments("--disable-setuid-sandbox");
         driver = new ChromeDriver(opt);
 
         String keyword = "test";

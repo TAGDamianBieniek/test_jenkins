7faa203b42d2f3ac2421e5506841acb76957ef95
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-06-24 14:06:17 +0200

    fixy

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -10,25 +10,29 @@ public class test {
 
     @Test
     public void mainTest(){
+        String x = "..............STRING............";
+        System.out.println(x);
 //        WebDriverManager.chromedriver().setup();
-//        WebDriver driver = new ChromeDriver();
-        File file = new File("/usr/local/bin/chromedriver");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+////        WebDriver driver = new ChromeDriver();
+//        File file = new File("/usr/local/bin/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//
+//
+//        ChromeOptions opt = new ChromeOptions();
+//        opt.setBinary("/usr/bin/google-chrome");
+//
+//        opt.addArguments("--headless");
+//        opt.addArguments("--window-size=1280x800");
+//        opt.addArguments("--no-sandbox");
+//        opt.addArguments("–-disable-dev-shm-usage");
+//        opt.addArguments("--start-maximized");
+//        opt.addArguments("--disable-gpu");
+//        opt.addArguments("--disable-setuid-sandbox");
+//        WebDriver driver = new ChromeDriver(opt);
 
 
-        ChromeOptions opt = new ChromeOptions();
-        opt.setBinary("/usr/bin/google-chrome");
-
-        opt.addArguments("--headless");
-        opt.addArguments("--window-size=1280x800");
-        opt.addArguments("--no-sandbox");
-        opt.addArguments("–-disable-dev-shm-usage");
-        opt.addArguments("--start-maximized");
-        opt.addArguments("--disable-gpu");
-        opt.addArguments("--disable-setuid-sandbox");
-        WebDriver driver = new ChromeDriver(opt);
-
-        driver.get("https://www.google.com");
-        System.out.println("Title: " + driver.getTitle());
+//
+//        driver.get("https://www.google.com");
+//        System.out.println("Title: " + driver.getTitle());
     }
 }

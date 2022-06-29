ed04e1bef4136459bc83fd0d2a2d73ea2f150a1f
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-06-29 13:53:00 +0200

    testt

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -20,6 +20,7 @@ public class test {
 
     @Test
     public void mainTest(){
+        System.out.println("TEST.................................................");
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
         File file = new File("/usr/local/bin/chromedriver");
@@ -29,16 +30,16 @@ public class test {
         ChromeOptions opt = new ChromeOptions();
         opt.setBinary("/usr/bin/google-chrome");
 
-        opt.addArguments("headless");
+        opt.addArguments("--headless");
 //        opt.addArguments("window-size=1280x800");
-        opt.addArguments("no-sandbox");
+        opt.addArguments("--no-sandbox");
         opt.addArguments("–disable-dev-shm-usage");
-        opt.addArguments("start-maximized");
+        opt.addArguments("--start-maximized");
         opt.addArguments("--disable-gpu");
         opt.addArguments("--disable-setuid-sandbox");
         driver = new ChromeDriver(opt);
 
-        System.out.println("TEST.................................................");
+
 
 
         String keyword = "Budowa";

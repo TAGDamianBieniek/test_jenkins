8a7aa80f4b0728df10fdd04d6487a7300ca44d82
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-07-04 14:00:13 +0200

    fix binary

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -32,7 +32,7 @@ public class test {
 
 
         ChromeOptions opt = new ChromeOptions();
-        opt.setBinary("/usr/bin/google-chrome");
+//        opt.setBinary("/usr/bin/google-chrome");
 
         opt.addArguments("--no-sandbox");
         opt.addArguments("--headless");

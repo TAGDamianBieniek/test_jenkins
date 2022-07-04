f3cb82d5c4b69c5074780afe1a750b9e141556ef
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-07-04 10:09:24 +0200

    fix binary

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -35,10 +35,10 @@ public class test {
         opt.addArguments("--no-sandbox");
         opt.addArguments("--headless");
 //        opt.addArguments("window-size=1280x800");
-        opt.addArguments("–-disable-dev-shm-usage");
-        opt.addArguments("start-maximized");
-        opt.addArguments("--disable-gpu");
-        opt.addArguments("--disable-setuid-sandbox");
+//        opt.addArguments("–-disable-dev-shm-usage");
+//        opt.addArguments("start-maximized");
+//        opt.addArguments("--disable-gpu");
+//        opt.addArguments("--disable-setuid-sandbox");
         driver = new ChromeDriver(opt);
 
         String keyword = "Budowa";

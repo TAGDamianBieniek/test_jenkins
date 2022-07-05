c9a05b49b12f09c7efc48eb99beb5928bf793594
TAGDamianBieniek <103108335+TAGDamianBieniek@users.noreply.github.com>
2022-07-05 09:17:22 +0200

    fix binary

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -32,7 +32,7 @@ public class test {
 
 
         ChromeOptions opt = new ChromeOptions();
-        opt.setBinary("/usr/bin/chromium-browser");
+        opt.setBinary("/usr/bin/google-chrome");
 
         opt.addArguments("--lang=en");
         opt.addArguments("--no-sandbox");
